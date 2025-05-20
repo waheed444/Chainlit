b8f59638a72d4a11b1d0dca0d30288f05ef34ab8
Waheed Ahmad <waheedahmad5519@gmail.com>
2025-05-20 07:51:44 +0500

    exploring chainlit

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@ import chainlit as cl
 
 @cl.on_message     # Use Decorator
 async def main(message: cl.Message):
-    # Your custom logic goes here...
 
     # Send a response back to the user
     await cl.Message(
-        content=f"Agent: {message.content}",).send()
+        content=f"Response: {message.content}",).send()
     
